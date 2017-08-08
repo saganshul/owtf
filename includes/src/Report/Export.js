bac191e06a3e028a2485a25e35a320b2012ec927
Anshul Singhal <sganshuliiith@gmail.com>
2017-08-08 17:13:46 +0530

    [Report] Adds multi template support

--- includes/src/Report/Export.js
+++ includes/src/Report/Export.js
@@ -1,13 +1,21 @@
 import Docxtemplater from 'docxtemplater';
 import {TARGET_API_URI} from '../constants.jsx';
 import JSZip from 'jszip';
-import template from './input.docx';
 import saveAs from 'save-as';
 
-// Funtion responsible for generating docx from JSON using docxtemplater.
-function getDocxReportFromJSON(json) {
-    var zip = new JSZip(template);
+function importAll(r) {
+  let templates = {};
+  r.keys().map((item, index) => { templates[item.replace('./', '')] = r(item); });
+  return templates;
+}
 
+const templates = importAll(require.context('./templates/', true, /\.(docx)$/));
+
+export const templatesNames = Object.keys(templates);
+
+// Funtion responsible for generating docx from JSON using docxtemplater.
+function getDocxReportFromJSON(json, template) {
+    var zip = new JSZip(templates[template]);
     var doc = new Docxtemplater();
     doc.loadZip(zip);
 
@@ -44,12 +52,12 @@ function getDocxReportFromJSON(json) {
   * docxtemplater takes a JSON and docx template and outputs output docx.
   */
 
-export function getDocx(target_id) {
+export function getDocx(target_id, template) {
     $.ajax({
         url: TARGET_API_URI + target_id + '/export/',
         type: 'GET',
         success: function(result) {
-            getDocxReportFromJSON(result);
+            getDocxReportFromJSON(result, template);
         },
         error: function(xhr, textStatus, serverResponse) {
             console.log("Server replied: " + serverResponse);
